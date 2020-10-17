e0026c154b68ba6e360d064b8c620340ff1ffe94
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-10-17 17:56:30 +0700

    first commit

---
+++ app/components/AppButton/AppButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, StyleSheet, View } from 'react-native'
+
+export default function AppButton() {
+    return (
+        <View>
+            <Button styles={}></Button>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
